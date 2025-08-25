3f4c2c847eace7b64b341179a29c32bba281ed97
Phil <irons7654321@gmail.com>
2025-08-25 22:07:53 +0800

    Initial Commit

---
+++ IRQuickTracking/IRQuickTrackingApp.swift
@@ -0,0 +1,17 @@
+//
+//  IRQuickTrackingApp.swift
+//  IRQuickTracking
+//
+//  Created by Phil on 2025/8/25.
+//
+
+import SwiftUI
+
+@main
+struct IRQuickTrackingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ IRQuickTrackingTests/IRQuickTrackingTests.swift
@@ -0,0 +1,17 @@
+//
+//  IRQuickTrackingTests.swift
+//  IRQuickTrackingTests
+//
+//  Created by Phil on 2025/8/25.
+//
+
+import Testing
+@testable import IRQuickTracking
+
+struct IRQuickTrackingTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
